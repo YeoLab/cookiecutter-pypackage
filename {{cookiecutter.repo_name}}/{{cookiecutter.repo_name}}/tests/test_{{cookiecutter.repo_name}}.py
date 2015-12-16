53b597da3ccfd44d00f75d97353b8f267b1b0574
Olga Botvinnik <olga.botvinnik@gmail.com>
2015-12-16 11:50:47 -0800

    Add example static data

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/tests/test_{{cookiecutter.repo_name}}.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/tests/test_{{cookiecutter.repo_name}}.py
@@ -8,11 +8,16 @@ test_{{ cookiecutter.repo_name }}
 Tests for `{{ cookiecutter.repo_name }}` module.
 """
 
+import random
 import pytest
 
 
 
 class Test{{ cookiecutter.repo_name|capitalize }}(unittest.TestCase):
+
+    @pytest.fixture(params=[2, 3])
+    def static_data(self, request):
+        return [random.randint() for _ in range(request.param)]
 
     def test_something(self):
         from {{ cookiecutter.repo_name }} import {{ cookiecutter.repo_name }}
